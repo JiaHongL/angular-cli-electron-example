1ac708f742e8e5cec9030af3c0def289a938853d
jiahongL <jiahongl@jiahongLdeMacBook-Pro.local>
2017-03-12 01:32:02 +0800

    angular cli 搭配 electron

---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+declare var electron: any;
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  constructor() { }
+  title = 'app works!';
+
+  openWeb() {
+    let url = 'https://electron.atom.io/';
+    electron.shell.openExternal(url);
+  }
+
+  openApplication() {
+    electron.remote.dialog.showOpenDialog({
+      properties: ['openFile', 'openDirectory']
+    }, (value) => {
+      let files = value;
+      if (typeof files != "undefined") {
+        electron.shell.openItem(files[0]);
+      }
+    });
+  }
+
+}
